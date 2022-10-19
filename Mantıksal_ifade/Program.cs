1d5dcdd2d37ddf7d1a6c5aacc1c32692b727a03a
Burak Can DURAN <BURAK.DURAN44@hotmail.com>
2022-10-19 22:49:58 +0300

    Add project files.

---
+++ Mantıksal_ifade/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int x = 20;
+int y = 13;
+int z = 42;
+
+bool islem = (23 == 55) && (76 > 45) && (5 < 12);
+Console.WriteLine(islem);
+bool islem1 = (23 >= 23) && (45 != 18);
+Console.WriteLine(islem1);
+
+bool islem2 = (x > y) && (z == y) && (z < x);
+Console.WriteLine(islem2);
+bool islem3 = (z > x) && (y < x);
+Console.WriteLine(islem3);
+bool islem4 = (x != z) || (y <= z);
+Console.WriteLine(islem4);
